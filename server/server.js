224e6d455e44d17f81a4733dc56ae403ff6abd3a
kwingham <kyle_wingham@outlook.com>
2024-09-29 08:34:57 +0100

    Intial setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Welcome to Swap Travel Stories API!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
